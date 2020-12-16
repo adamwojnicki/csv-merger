f85b5936f9f205592e631788003dbe354213f17c
Adam Wojnicki <wojnicki.adam1@gmail.com>
2020-12-16 19:37:46 +0100

    merging csv files and dropping duplicates

--- csv-merger.py
+++ csv-merger.py
@@ -3,8 +3,20 @@ import os
 
 # LOOKING FOR CSV FILE IN DIRECTORY
 all_files = os.listdir() 
-csv_files = []
+data_frames = []
+first_file = True
+out_file = 'merged.csv'
+final_file = 'final_merged.csv'
 for file in all_files:
     if file.endswith('.csv'):
-        csv_files.append(file) 
-print(csv_files)
+        df = pd.read_csv(file)
+        df = df.dropna()
+        if first_file:
+            df.to_csv(out_file, index=False) 
+            first_file = False
+        else:
+            df.to_csv(out_file, index=False, header=False, mode='a')
+
+merged = pd.read_csv(out_file)
+no_duplcates = merged.drop_duplicates()
+no_duplcates.to_csv(final_file, index=False)
