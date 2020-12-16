48176ef084679ba863ee1fa1b84273eb41505240
Adam Wojnicki <wojnicki.adam1@gmail.com>
2020-12-16 21:03:49 +0100

    removing unnecessary code and adding guiding comments

--- csv-merger.py
+++ csv-merger.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import os
 
-# LOOKING FOR CSV FILE IN DIRECTORY
+# INITIAL SETTINGS
 all_files = os.listdir() 
-data_frames = []
 first_file = True
 out_file = 'merged.csv'
 final_file = 'final_merged.csv'
+# APPENDING DATA TO NEW CSV FILE
 for file in all_files:
     if file.endswith('.csv') and not file.endswith('merged.csv'):
         df = pd.read_csv(file)
@@ -17,6 +17,7 @@ for file in all_files:
         else:
             df.to_csv(out_file, index=False, header=False, mode='a')
 
+# CLEARING FROM DUPLACATES
 merged = pd.read_csv(out_file)
 no_duplcates = merged.drop_duplicates()
 no_duplcates.to_csv(final_file, index=False)
