1e77a2b40585d995a03fffa2a134d4d2316a3ccc
Adam Wojnicki <wojnicki.adam1@gmail.com>
2020-12-16 21:02:04 +0100

    merged.csv overflow bug fixed

--- csv-merger.py
+++ csv-merger.py
@@ -8,7 +8,7 @@ first_file = True
 out_file = 'merged.csv'
 final_file = 'final_merged.csv'
 for file in all_files:
-    if file.endswith('.csv'):
+    if file.endswith('.csv') and not file.endswith('merged.csv'):
         df = pd.read_csv(file)
         df = df.dropna()
         if first_file:
